55c68896aa5f395ea1f401c051ab47d3aa9a901b
drose123456 <acg_danial@hotmail.com>
2023-03-15 12:01:38 +0000

    added routes for pizzas and passing information using hbs, implemented some basic views, partials and layouts

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 
 const app = express();
 
+app.set("views", __dirname + "/views"); //tells our Express app where to look for our views
+app.set("view engine", "hbs"); //sets HBS as the template engine
+
+
 // Make everything inside of public/ available
 app.use(express.static('public'));
 
@@ -9,14 +13,54 @@ app.use(express.static('public'));
 
 //Route for homepage
 app.get("/", (request, response, next) => {
-    response.sendFile(__dirname + '/views/home.html');
+    response.render("home");
 });
 
 
 //Route for contact page
 app.get("/contact", (request, response, next) => {
-    response.sendFile(__dirname + '/views/contact-page.html');
+    response.render("contact-page");
 })
+
+
+//GET /pizzas/margarita
+app.get("/pizzas/margarita", (request, response, next) => {
+
+    const data = {
+        title: 'Pizza Margarita',
+        price: 12,
+        imageFile: 'pizza-margarita.jpg',
+        ingredients: ['mozzarella', 'tomato sauce', 'basilicum'],
+    };
+
+    response.render("product", data);
+});
+
+
+//GET /pizzas/veggie
+app.get("/pizzas/veggie", (request, response, next) => {
+
+    const data = {
+        title: 'Veggie Pizza',
+        price: 15,
+        imageFile: 'pizza-veggie.jpg',
+        ingredients: ['cherry tomatoes', 'basilicum', 'Olives'],
+    };
+    
+    response.render("product", data);
+});
+
+
+//GET /pizzas/seafood
+app.get("/pizzas/seafood", (request, response, next) => {
+
+    const data = {
+        title: 'Seafood Pizza',
+        imageFile: 'pizza-seafood.jpg',
+    };
+
+    response.render("product", data);
+});
 
 
 
